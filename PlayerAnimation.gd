461e47479fe9572be9ac7da59f3b4cec3439fa0a
Dariusz Wlodarz <toadstyle@Dariuszs-MacBook-Pro.local>
2023-02-22 20:58:35 +0100

    init

---
+++ PlayerAnimation.gd
@@ -0,0 +1,19 @@
+extends AnimatedSprite
+
+
+func _ready():
+	pass
+
+
+func _on_Player_animate(motion):
+	if motion.y < 0:
+		
+		play("jump")
+	elif motion.x > 0:
+		play("walk")
+		flip_h = false
+	elif motion.x < 0:
+		play("walk")
+		flip_h = true
+	else:
+		play("idle")
